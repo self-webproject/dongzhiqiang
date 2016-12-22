039c249ded2b1a2fded3f3678838634ffa271001
DONGZHIQIANG [董志强] <DONGZHIQIANG@kingsoft.com>
2016-12-22 11:42:02 +0800

    项目初始化

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global myproject, $*/
+
+
+window.myproject = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function () {
+    'use strict';
+    console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function () {
+  'use strict';
+  myproject.init();
+});
